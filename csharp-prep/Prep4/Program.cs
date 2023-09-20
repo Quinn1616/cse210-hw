7c7ffbc96796a88b1e0f9287c3b0ec532203efb7
Quinn Bastian <jacobqbastian@gmail.com>
2023-09-20 10:25:49 -0600

    Added Code to Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0) {
+            Console.Write("Enter Number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0){
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = 0;
+
+        foreach (int number in numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+
+
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
+
+
     }
 }
