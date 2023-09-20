4736e0d3c8d3529ceef232e4a01443a8c3f668a7
Quinn Bastian <jacobqbastian@gmail.com>
2023-09-20 10:45:32 -0600

    Added Code to Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptuserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(name, square);
+
+
+
+        static void DisplayWelcome() {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName() {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptuserNumber() {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int SquareNumber(int number) {
+            return number * number;
+        }
+
+        static void DisplayResult(string name, int number) {
+            Console.WriteLine($"{name}, the square of your number is {number}");
+        }
+        
+
     }
 }
