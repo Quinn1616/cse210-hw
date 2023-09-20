3d2284e941adad04977f5b56c6841acf550e185e
Quinn Bastian <jacobqbastian@gmail.com>
2023-09-20 09:47:55 -0600

    Added Code to Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess;
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        //Console.Write("What is the magic number? ");
+        //int number = int.Parse(Console.ReadLine());
+
+        do {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < number) {
+                Console.WriteLine("Higher");
+            }
+            if (guess > number) {
+                Console.WriteLine("Lower");
+            }
+        } while (guess != number);
+
+        Console.WriteLine("Correct");
+
+    
+
+        
+
+
+
+
     }
 }
