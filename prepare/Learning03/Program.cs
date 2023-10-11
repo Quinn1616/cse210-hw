2944cce43c7ea3155e6a45abd690bce71c298f0f
Quinn Bastian <jacobqbastian@gmail.com>
2023-10-11 15:15:14 -0600

    W05 Work

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        //Console.Write($"{fraction1.GetTop()}/");
+        //Console.WriteLine(fraction1.GetBottom());
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(6);
+        //Console.Write($"{fraction2.GetTop()}/");
+        //Console.WriteLine(fraction2.GetBottom());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(6, 7);
+        //Console.Write($"{fraction3.GetTop()}/");
+        //Console.WriteLine(fraction3.GetBottom());
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+        
     }
 }
