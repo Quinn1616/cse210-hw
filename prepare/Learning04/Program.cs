08a2daa62a2242083a5445e9e52995e3f0136968
Quinn Bastian <jacobqbastian@gmail.com>
2023-10-24 15:41:43 -0600

    W07 Work

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,8 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        MathAssignment mathAssignment = new MathAssignment("Quinn Bastian", "Computer Science", "7.3", "8-19");
+        Assignment assignment = new Assignment("Quinn Bastian", "Computer Science");
+        Console.WriteLine($"{assignment.GetSummary()}\n");
 
-        Console.WriteLine($"{mathAssignment.GetSummary()} \n{mathAssignment.GetHomeworkList()}");
+        MathAssignment mathAssignment = new MathAssignment("Quinn Bastian", "Math", "7.3", "8-19");
+        Console.WriteLine($"{mathAssignment.GetSummary()} \n{mathAssignment.GetHomeworkList()}\n");
+
+        WritingAssignment writingAssignment = new WritingAssignment("Quinn Bastian", "English", "MockingJay");
+        Console.WriteLine($"{writingAssignment.GetSummary()} \n{writingAssignment.GetWritingInformation()}");
     }
 }
